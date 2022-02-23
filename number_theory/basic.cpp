cf28e79b0b3361250fbd527729d5b185bb7d5925
fun-vivek <codwithvivekpandey@gmail.com>
2022-02-23 19:56:29 +0530

    number theory working

---
+++ number_theory/basic.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define F first
+#define S second
+#define LB lower_bound
+#define UB upper_bound
+#define all(v) v.begin(),v.end()
+#define REP(i,a,b) for(int i=a; i<=b; i++)
+void file(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+}
+// write your code from here
+
+
+int main(){
+  // ios::sync_with_stdio(0);
+  // cin.tie(0);
+  // file()
+  
+  return 0;
+}
